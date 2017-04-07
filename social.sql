968527491b702c5a67daa3d589e7315336025bf1
ThePatrykOOO <pfilipiak00@gmail.com>
2017-04-07 18:50:32 +0200

    Update social.sql

--- social.sql
+++ social.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Czas generowania: 26 Mar 2017, 10:15
+-- Czas generowania: 07 Kwi 2017, 18:49
 -- Wersja serwera: 10.1.16-MariaDB
 -- Wersja PHP: 7.0.9
 
@@ -19,6 +19,65 @@ SET time_zone = "+00:00";
 --
 -- Baza danych: `social`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `saved`
+--
+
+CREATE TABLE `saved` (
+  `idsave` int(11) NOT NULL,
+  `name_save` varchar(50) COLLATE utf8_polish_ci NOT NULL,
+  `url` text COLLATE utf8_polish_ci NOT NULL,
+  `iduser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `saved`
+--
+
+INSERT INTO `saved` (`idsave`, `name_save`, `url`, `iduser`) VALUES
+(1, 'Portfolio', 'http://patrykfilipiak.pl/', 2),
+(2, 'Fejs', 'https://www.facebook.com/', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `sites`
+--
+
+CREATE TABLE `sites` (
+  `idsite` int(11) NOT NULL,
+  `name` varchar(50) COLLATE utf8_polish_ci NOT NULL,
+  `description` varchar(300) COLLATE utf8_polish_ci NOT NULL,
+  `type` int(11) NOT NULL,
+  `admin` int(11) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `type_site`
+--
+
+CREATE TABLE `type_site` (
+  `id` int(11) NOT NULL,
+  `name_type` varchar(50) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `type_site`
+--
+
+INSERT INTO `type_site` (`id`, `name_type`) VALUES
+(1, 'Firma'),
+(2, 'Organizacja'),
+(3, 'Marka'),
+(4, 'Produkt'),
+(5, 'Artysta'),
+(6, 'Zespół');
 
 -- --------------------------------------------------------
 
@@ -46,11 +105,30 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `surname`, `email`, `password`, `birth`, `home`, `work`, `school`, `phone`, `about`, `status`) VALUES
-(1, 'Patryk', 'Filipiak', 'thepatrykooo@gmail.com', '$2y$10$zDroiVnDBqZILJP8QMWan.9wSQ49/AIy6FXJz0.tgKaJfGCe/keLm', '2010-11-29', NULL, NULL, NULL, NULL, NULL, 0);
+(1, 'Patryk', 'Filipiak', 'thepatrykooo@gmail.com', '$2y$10$DT5USNr67/gHAGrxGQj8ueoXUl4QwsO1mX0pkgiTYYNrve7WMc45O', '2000-01-02', 'Explorer', 'Programista HTML', 'ILO ILO', '736657933', 'Jestem Patryk kappa Programista HTML Kappa', 0),
+(2, 'Jan', 'Kowalski', 'patrykowegry1@gmail.com', '$2y$10$JXxHaIaPHxItellEklmpFebmZ5zQaVPKQTgehzLmqQ3ZrCXQYj7xO', '1999-02-23', NULL, NULL, NULL, NULL, NULL, 0);
 
 --
 -- Indeksy dla zrzutów tabel
 --
+
+--
+-- Indexes for table `saved`
+--
+ALTER TABLE `saved`
+  ADD PRIMARY KEY (`idsave`);
+
+--
+-- Indexes for table `sites`
+--
+ALTER TABLE `sites`
+  ADD PRIMARY KEY (`idsite`);
+
+--
+-- Indexes for table `type_site`
+--
+ALTER TABLE `type_site`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -62,6 +140,21 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT dla tabeli `saved`
+--
+ALTER TABLE `saved`
+  MODIFY `idsave` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT dla tabeli `sites`
+--
+ALTER TABLE `sites`
+  MODIFY `idsite` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT dla tabeli `type_site`
+--
+ALTER TABLE `type_site`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT dla tabeli `users`
 --
